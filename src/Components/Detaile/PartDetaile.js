0aa98991552a62ba2a735874e9a0b5669fe23081
mrbuhamad <mr.buhamad@hotmail.com>
2020-05-01 09:54:03 +0300

    debugging buield

--- src/Components/Detaile/PartDetaile.js
+++ src/Components/Detaile/PartDetaile.js
@@ -22,7 +22,7 @@ import Holds from "../Holds/Holds";
 
 // Modals
 import LoanModal from "../Modal/LoanModal";
-import HoldModal from "../Modal/HoldModal";
+// import HoldModal from "../Modal/HoldModal";
 
 class PartDetaile extends Component {
   participants = this.props.participants;
@@ -43,7 +43,8 @@ class PartDetaile extends Component {
 
   choosemodal = () => {
     if (holdStore.showHoldModal) {
-      return <HoldModal partID={this.partID} />;
+      // return <HoldModal partID={this.partID} />;
+      return null;
     } else {
       return <LoanModal partID={this.partID} />;
     }
